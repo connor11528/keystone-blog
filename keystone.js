6ffc778f87019fe537b5e52af6cb6a2584a4e7bf
Connor Leech <connorleech@gmail.com>
2016-01-31 18:52:24 -0800

    production environment

--- keystone.js
+++ keystone.js
@@ -95,8 +95,11 @@ keystone.set('nav', {
 	'users': 'users'
 });
 
+// set values from .env for production environment
 if (keystone.get('env') == 'production'){
-	keystone.set('cloudinary config', process.env.CLOUDINARY_URL );
+	keystone.set('cloudinary config', process.env.CLOUDINARY_URL);
+	keystone.set('cookie secret', process.env.COOKIE_SECRET);
+	keystone.set('mandrill api key', process.env.MANDRILL_API_KEY);
 }
 
 // Start Keystone to connect to your database and initialise the web server
